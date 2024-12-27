0d21ae6cba11ae60daacf25f0cfad84178f2d2db
Manisharma K <129358155+Manisharma-k@users.noreply.github.com>
2024-12-27 12:23:05 +0530

    Add files via upload

---
+++ 4-bit-seq_tb.v
@@ -0,0 +1,24 @@
+module bcd_tb();
+reg clk;     
+reg rst;   
+wire [3:0] ones;
+wire [3:0] tens; 
+bcd_count dut(clk,rst,ones,tens);
+initial 
+begin
+clk=0;
+forever #10 clk = ~clk; 
+end
+initial 
+begin   
+clk = 0;
+rst = 1;       
+#10 rst = 0;
+#2000 $stop;
+end
+initial 
+begin
+$monitor("Time = %t, tens = %d ,ones = %d",$time, tens, ones);
+end
+endmodule
+
